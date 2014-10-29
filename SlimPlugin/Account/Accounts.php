5e0af91aadceaeecf8d4f05474dc629e0a6e0aa1
Soeren <soeren@siegismund-poschmann.de>
2014-10-29 12:32:09 +0100

    Make isLogin public

--- SlimPlugin/Account/Accounts.php
+++ SlimPlugin/Account/Accounts.php
@@ -111,7 +111,7 @@ class Accounts extends \SSP\Mongo\Model\Basic\MongoModel
 	    }
 	    return $randomString;
 	}
-	private static function isLogin()
+	public static function isLogin()
 	{
 		if(isset($_SESSION["id"]) && $_SESSION["id"]!==false)
 		{
